88762cebc68729106baaab7b1cf0bb2c64889164
Hank <dashing.hank@gmail.com>
2024-12-26 18:08:52 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div class="flex justify-center bg-black">
+    <img class="w-full max-w-[1100px]" src="./assets/eusd-landingpage.jpg" />
+  </div>
+</template>
+
+<style scoped>
+</style>
